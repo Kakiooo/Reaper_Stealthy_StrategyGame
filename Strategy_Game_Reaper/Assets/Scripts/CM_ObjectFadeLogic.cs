eb14ab70064678bd6576518629fbceb7ad00d9e3
Kakiooo <johnnyjarrah@163.com>
2025-11-10 17:38:36 -0800

    updating

--- Strategy_Game_Reaper/Assets/Scripts/CM_ObjectFadeLogic.cs
+++ Strategy_Game_Reaper/Assets/Scripts/CM_ObjectFadeLogic.cs
@@ -7,6 +7,7 @@ public class CM_ObjectFadeLogic : MonoBehaviour
     [Header("Assign Values")]
     Vector3 _cameraDir;
     [SerializeField] GameObject _p_Object;
+    [SerializeField] LayerMask _wallLayer;
     float _rayLength;
     RaycastHit _hit;
     public bool CMStartFade;
@@ -21,19 +22,23 @@ public class CM_ObjectFadeLogic : MonoBehaviour
     {
         _cameraDir=_p_Object.transform.position-transform.position;
         _rayLength=_cameraDir.magnitude;
+
         if (Physics.Raycast(transform.position,_cameraDir, out _hit, _rayLength))
         {
-                 
-            if (_hit.transform.tag == "Player")
+            if (_hit.transform.tag == "Wall")
             {
-                if(_wallBlocking!=null) _wallBlocking.StartFade = false;
-            }
-            else if (_hit.transform.tag == "Wall")
-            {
-                _wallBlocking = _hit.transform.gameObject.GetComponent<Obstacles_Fading>();
+                print("Working?");
+                //_wallBlocking = _hit.transform.gameObject.GetComponent<Obstacles_Fading>();
+                _hit.transform.gameObject.GetComponent<MeshRenderer>().enabled = false;
                 CurrentHitObject_Name = _hit.transform.gameObject.name;
-                _wallBlocking.StartFade = true;
+                //_wallBlocking.StartFade = true;
             }
+            else if (_hit.transform.tag == "Player")
+            {
+                //if(_wallBlocking!=null) _wallBlocking.StartFade = false;
+                _hit.transform.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+
         }
 
     }
