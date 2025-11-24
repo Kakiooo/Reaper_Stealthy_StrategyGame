f33d20faa5a133725295005893fa8716a5c6c829
Kakiooo <johnnyjarrah@163.com>
2025-11-24 00:10:26 -0800

    MenuFinished

--- Strategy_Game_Reaper/Assets/Scripts/GameManager.cs
+++ Strategy_Game_Reaper/Assets/Scripts/GameManager.cs
@@ -13,18 +13,8 @@ public class GameManager : MonoBehaviour
     public bool LoseLevel;
     public float CountDown;
     public float Timer_Result;
-    public float CountDownRolling;
     public PlayerManager PlayerManager;
     public Ending_DisplayResult DisplayResult;
-    public RectTransform IntroPivot;
-    public RectTransform Intro_Panel;
-    public bool IsOn;
-    public bool StartRolling;
-    public CinemachineVirtualCamera FirstCM;
-    public CinemachineVirtualCamera SecondCM;
-    public Animator FridgeAnimator;
-    public GameObject SpotLight_Board;
-    Vector2 _formor_IntroPos;
 
     public enum GameState
     {
@@ -41,7 +31,6 @@ public class GameManager : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
-        _formor_IntroPos = Intro_Panel.anchoredPosition;
         DontDestroyOnLoad(this);
     }
 
@@ -51,7 +40,6 @@ public class GameManager : MonoBehaviour
         EndGamePhase();
         MissionTimer();
         GameState_Determine();
-        CameraRolling();
     }
 
     void GameState_Determine()
@@ -104,50 +92,6 @@ public class GameManager : MonoBehaviour
             PlayerManager.CurrentState = PlayerManager.PlayerState.EndGame;
             CurrentState = GameState.EndPhase;
         }
-    }
-
-
-    public void StartGame()
-    {
-        if (FirstCM.gameObject != null && FridgeAnimator.gameObject != null)
-        {
-            FridgeAnimator.SetBool("IsOpen", true);
-            FirstCM.enabled = false;
-            StartRolling=true;
-        }
-
-    }
-    public void CameraRolling()
-    {
-        if (StartRolling)
-        {
-            CountDownRolling += Time.deltaTime;
-            if (CountDownRolling >= 3)
-            {
-                SecondCM.enabled = false;
-                SpotLight_Board.gameObject.SetActive(true);
-                CountDownRolling = 0;
-                StartRolling =false;
-            }
-        }
-    }
-
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    public void ShowInstructionTab()
-    {
-        IsOn = !IsOn;
-        if(IsOn) Intro_Panel.DOAnchorPos(IntroPivot.anchoredPosition, 0.5f);
-        else Intro_Panel.DOAnchorPos(_formor_IntroPos, 0.5f);
-    }
-
-    public void InstructionEnd_SwitchScene()
-    {
-        SceneManager.LoadScene("InGame");
     }
 
     
---
+++ Strategy_Game_Reaper/Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,88 @@
+using Cinemachine;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLogic : MonoBehaviour
+{
+    public float CountDownRolling;
+    public RectTransform IntroPivot;
+    public RectTransform Intro_Panel;
+    public RectTransform MenuGroup;
+    public bool IsOn;
+    public bool StartRolling;
+    public CinemachineVirtualCamera FirstCM;
+    public CinemachineVirtualCamera SecondCM;
+    public CinemachineBrain Brain;
+    public Animator FridgeAnimator;
+    public GameObject SpotLight_Board;
+    public RectTransform HidePos;
+    Vector2 _formor_IntroPos;
+    Vector2 _menuTab_Pos;
+
+    private void Awake()
+    {
+        _formor_IntroPos = Intro_Panel.anchoredPosition;
+        _menuTab_Pos= MenuGroup.anchoredPosition;   
+    }
+
+    private void Update()
+    {
+        CameraRolling();
+    }
+
+    public void StartGame()
+    {
+        if (FirstCM.gameObject != null && FridgeAnimator.gameObject != null)
+        {
+            FridgeAnimator.SetBool("IsOpen", true);
+            FirstCM.enabled = false;
+            StartRolling = true;
+            MenuGroup.DOAnchorPos(HidePos.anchoredPosition, 0.5f);
+        }
+
+    }
+    public void CameraRolling()
+    {
+        if (StartRolling)
+        {
+            CountDownRolling += Time.deltaTime;
+            if (CountDownRolling >= 3)
+            {
+                SecondCM.enabled = false;
+                CountDownRolling = 0;
+                StartRolling = false;
+            }
+        }
+        else if (Brain.IsBlending == false && SecondCM.isActiveAndEnabled == false)
+        {
+            SpotLight_Board.gameObject.SetActive(true);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowInstructionTab()
+    {
+        IsOn = !IsOn;
+        if (IsOn) Intro_Panel.DOAnchorPos(IntroPivot.anchoredPosition, 0.5f);
+        else Intro_Panel.DOAnchorPos(_formor_IntroPos, 0.5f);
+    }
+
+    public void InstructionEnd_SwitchScene()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void LoadCutScene()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+
+}
